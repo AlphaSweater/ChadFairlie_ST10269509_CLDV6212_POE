4d0d3fa206654e1a32af4cd9a87652009bd0bacd
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-08-10 16:47:00 +0200

    Add Product management features
    Created ProductController with Index and Manage actions.
    
    Added ProductViewModel class to represent product data.
    
    Added Index.cshtml and Manage.cshtml views for product listing and management.

---
+++ ABC_Retail/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ABC_Retail.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABC_Retail.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			// Fetch product list (simplified example)
+			var products = new List<ProductViewModel>
+			{
+				new ProductViewModel { Id = 1, Name = "Product A", Price = 9.99m, Description = "Description for Product A" },
+				new ProductViewModel { Id = 2, Name = "Product B", Price = 19.99m, Description = "Description for Product B" }
+			};
+			return View(products);
+		}
+
+		public IActionResult Manage(int id)
+		{
+			// Fetch product details by ID (simplified example)
+			var product = new ProductViewModel { Id = id, Name = "Product A", Price = 9.99m, Description = "Description for Product A" };
+			return View(product);
+		}
+	}
+}
